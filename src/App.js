2ef178b371904a7dae15c55736a51516ffb6f96a
Ryan Jarnutowski <rjarnutowski@bluestatedigital.com>
2018-04-11 13:17:22 -0400

    adding config.example

--- src/App.js
+++ src/App.js
@@ -16,8 +16,6 @@ import './App.css';
 /** MATERIAL UI **/
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import theme from 'theme.js';
-/** REDOC **/
-
 
 const hercTheme = createMuiTheme(theme);
 
---
+++ src/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    firebase: {
+        apiKey: '',
+        authDomain: 'herc-ui.firebaseapp.com',
+        databaseURL: 'https://herc-ui.firebaseio.com',
+        projectId: 'herc-ui',
+        storageBucket: '',
+        messagingSenderId: '',
+    },
+};
